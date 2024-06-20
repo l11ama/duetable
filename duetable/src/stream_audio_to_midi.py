9a4c6e8dd87ba1434aa67e79cd96d9ffbe969986
jerzy.mizgiert <jerzy.mizgiert@native-instruments.de>
2024-06-20 15:38:45 +0200

    acts

--- duetable/src/stream_audio_to_midi.py
+++ duetable/src/stream_audio_to_midi.py
@@ -1,9 +1,8 @@
-import random
 import warnings
 from concurrent.futures import ThreadPoolExecutor
 from pprint import pprint
 from time import time
-from typing import List, Optional
+from typing import List
 
 import pyaudio
 import numpy as np
@@ -12,15 +11,11 @@ from miditoolkit.midi import containers as ct
 from mido import second2tick, bpm2tempo
 from numpy import median, diff
 
-from duetable.src.audio_to_midi_aub import AudioToMidiWithAubio
-# from duetable.src.audio_to_midi_spoti import AudioToMidiWithSpotify
-from duetable.src.midi_devices import log_input_output_devices, open_output
 from duetable.src.interfaces import AudioToMidi, MidiBufferRegenerator, MidiBufferPostTransformation
 from duetable.src.midi_utils import MIDI_DATA_BY_NO
-from duetable.src.regenerators import DummyRegenerator, HttpMuptRegenerator, MuptWithMarkovChainRegenerator
+from duetable.src.regenerators import DummyRegenerator
 from duetable.src.sequence_player import SequencePlayer
 from duetable.src.settings import DuetableSettings, RecordingStrategy
-from duetable.src.transformers import SimpleTransposeTransformer, SimpleTimeTransformer, MidiRangeTransformer
 
 
 class DuetableThreadPoolExecutor(ThreadPoolExecutor):
@@ -311,49 +306,3 @@ class StreamAudioToMidi:
 
 warnings.filterwarnings('ignore')
 
-# ======================================================
-log_input_output_devices()
-open_output('Elektron Model:Cycles')
-open_output('Duetable Bus 1')
-
-settings = DuetableSettings()
-settings.buffer_length = 12
-settings.buffer_time = 4.0
-settings.recording_strategy = RecordingStrategy.TIME
-settings.record_when_playing = False
-settings.append_to_play_buffer = False
-
-settings.upper_meter = 4
-settings.lower_meter = 4
-settings.bpm = 120
-
-settings.n_bars = 2
-settings.temperature = 0.8
-settings.model_size = "large"
-
-settings.loop_playback = False
-
-stream_2_midi = StreamAudioToMidi(
-    # midi converter
-    converter=AudioToMidiWithAubio(down_sample=1),
-    # hop_s=10*2048,  # set for Spotify due to natural network nature for prediction, comment out for Aubio
-    # converter=AudioToMidiWithSpotify(),
-
-    # setting
-    settings=settings,
-
-    # audio in
-    # device_name="U46",
-
-    # detected midi regenerator
-    regenerator=HttpMuptRegenerator(),
-    # regenerator=MuptWithMarkovChainRegenerator(),
-
-    # post transformers
-    transformations=[
-        # SimpleTransposeTransformer(lambda: random.randint(-12, 12)),
-        # SimpleTimeTransformer(lambda: random.randint(5, 35)/10)
-        # MidiRangeTransformer(from_midi_no=26, to_midi_no=90)
-    ]
-)
-stream_2_midi.read()
